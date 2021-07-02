fc4a32514a28256bc5e42de12eddab45f3e03161
tkd9714 <tkd9714@hotmail.com>
2021-07-02 16:27:32 +0800

    first commit

---
+++ src/Contract/BBINServiceInterface.php
@@ -0,0 +1,93 @@
+<?php
+declare(strict_types=1);
+namespace GiocoPlus\BBIN\Contract;
+/**
+ * BBIN
+ */
+interface BBINServiceInterface {
+
+    /**
+     * 遊戲啟動
+     *
+     * @param string $opCode
+     * @param string $account
+     * @param string $gameCode
+     * @param string $language
+     * @param string $vendorCode
+     * @param string $backUrl
+     * @return mixed
+     */
+    function gameLaunch(string $opCode, string $account, string $gameCode, string $language, string $vendorCode, string $backUrl = null);
+
+    /**
+     * 遊戲詳情
+     * @param string $opCode
+     * @param string $account
+     * @param string $betId
+     * @param string $vendorCode
+     * @return mixed
+     */
+    function gameDetail(string $opCode, string $account, string $betId, string $vendorCode);
+
+    /**
+     * 抓取遊戲紀錄
+     *
+     * @param string $opCode
+     * @param integer $pastMinutes
+     * @param string $cacheKey
+     * @param string $vendorCode
+     * @return mixed
+     */
+    function betLogGrabber(string $opCode, int $pastMinutes, string $cacheKey, string $vendorCode);
+
+    /**
+     * 全營商 抓取遊戲紀錄
+     *
+     * @param integer $pastMinutes
+     * @param string $vendorCode
+     * @return mixed
+     */
+    function betLogGrabberAll(int $pastMinutes, string $vendorCode);
+
+    /**
+     * 遊戲上分
+     *
+     * @param string $opCode
+     * @param string $account
+     * @param string $vendorCode
+     * @return mixed
+     */
+    function gameTransferIn(string $opCode, string $account, string $vendorCode);
+
+    /**
+     * 遊戲下分
+     *
+     * @param string $opCode
+     * @param string $account
+     * @param float $amount
+     * @param string $vendorCode
+     * @return mixed
+     */
+    function gameTransferOut(string $opCode, string $account, float $amount, string $vendorCode);
+
+    /**
+     * 取得錢包餘額
+     *
+     * @param string $opCode
+     * @param string $account
+     * @param string $vendorCode
+     * @return mixed
+     */
+    function getBalance(string $opCode, string $account, string $vendorCode);
+
+    /**
+     * 上/下分失敗 訂單檢核
+     *
+     * @param string $opCode
+     * @param string $orderNo
+     * @param string $vendorCode
+     * @return mixed
+     */
+    function orderFailCheck(string $opCode, string $orderNo, string $vendorCode);
+}
+
